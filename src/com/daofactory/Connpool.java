eb0d85834a213669a0fdcf323917a70550f0e45e
dong <873546384@qq.com>
2017-06-21 21:40:54 +0800

    6.21dhw提交

--- src/com/daofactory/Connpool.java
+++ src/com/daofactory/Connpool.java
@@ -14,7 +14,6 @@ public class Connpool {
 	}
 	private static BasicDataSource dataSource=null;
 	static{
-<<<<<<< HEAD
 		
 		dataSource=new BasicDataSource();
 		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -22,13 +21,6 @@ public class Connpool {
 		dataSource.setUsername("sa");
 		dataSource.setPassword("818716");
 		
-=======
-		bds = new BasicDataSource();
-		bds.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		bds.setUrl("jdbc:sqlserver://172.16.22.78:1433; DatabaseName=order");
-		bds.setUsername("sa");
-		bds.setPassword("818716");
->>>>>>> 7cefe3b6e14880f74adc48f4cb7ccbcf4e7beddb
 	}
 	public Connection getConnection(){
 
